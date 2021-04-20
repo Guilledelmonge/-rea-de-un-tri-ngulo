202afceaa38f0b3c675193a0adee71aa4417f2fb
Guillermo Delgado <>
2021-04-20 16:29:57 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  int base;
+  int altura;
+
+std::cout << "digite la base: " << std ::endl;
+std::cin  >> base;
+
+std::cout << "digite la altura: " << std ::endl;
+std::cin >> altura;
+
+  std::cout << "El área del triángulo es " << ((base*altura)/2) << " metros"<< std:: endl;
+
+
+}
